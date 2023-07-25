dd322451fcaa7bdfec670410a864a48d5d976e30
crupy <wlxh95@naver.com>
2023-07-25 09:34:33 +0900

    [Feat] 근태관리 프로젝트 백엔드 세팅
    근태관리 프로젝트 백엔드 기본 세팅

---
+++ src/main/java/com/douzone/timeattendance/TimeAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.timeattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAttendanceApplication.class, args);
+	}
+
+}
