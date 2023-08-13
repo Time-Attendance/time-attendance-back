66d2eed6a696a5947cf6b4d1fddea1e982d5adfa
hunbk <shyun823@gmail.com>
2023-08-13 16:08:40 +0900

    [Refactor] 올바르지 않은 퇴근 요청에 대한 예외 메시지 변경

--- src/main/java/com/douzone/timeattendance/exception/timerecord/ImpossibleLeaveWorkException.java
+++ src/main/java/com/douzone/timeattendance/exception/timerecord/ImpossibleLeaveWorkException.java
@@ -3,11 +3,11 @@ package com.douzone.timeattendance.exception.timerecord;
 import com.douzone.timeattendance.exception.TimeAttendanceException;
 import org.springframework.http.HttpStatus;
 
-public class InvalidLeaveWorkException extends TimeAttendanceException {
+public class ImpossibleLeaveWorkException extends TimeAttendanceException {
 
-    private static final String MESSAGE = "오늘의 출근 기록이 존재하지 않습니다.";
+    private static final String MESSAGE = "오늘의 퇴근 기록을 저장할 출근 기록이 존재하지 않습니다.";
 
-    public InvalidLeaveWorkException() {
+    public ImpossibleLeaveWorkException() {
         super(MESSAGE);
     }
 
