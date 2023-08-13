ec12fae4fff2b8e27cfaa05e6ed1bfe390edf5d9
hunbk <shyun823@gmail.com>
2023-08-13 16:11:07 +0900

    [Refactor] 인증 로직 변경에 따른 출퇴근 기능 수정
    - 사용자 ID를 기반으로 출퇴근 비즈니스 로직 처리

--- src/main/java/com/douzone/timeattendance/controller/TimeRecordController.java
+++ src/main/java/com/douzone/timeattendance/controller/TimeRecordController.java
@@ -1,6 +1,6 @@
 package com.douzone.timeattendance.controller;
 
-import com.douzone.timeattendance.dto.auth.LoginUserEmail;
+import com.douzone.timeattendance.dto.auth.AuthInfo;
 import com.douzone.timeattendance.dto.timerecord.TimeRecordResponse;
 import com.douzone.timeattendance.global.auth.LoginUser;
 import com.douzone.timeattendance.service.TimeRecordService;
@@ -23,15 +23,15 @@ public class TimeRecordController {
     private final TimeRecordService timeRecordService;
 
     @PostMapping("/work")
-    public ResponseEntity<Void> startWork(@LoginUser LoginUserEmail loginUserEmail) {
-        timeRecordService.startWork(loginUserEmail.getEmail());
+    public ResponseEntity<Void> startWork(@LoginUser AuthInfo authInfo) {
+        timeRecordService.startWork(authInfo.getUserId());
         return ResponseEntity.status(HttpStatus.CREATED)
                              .build();
     }
 
     @PostMapping("/leave")
-    public ResponseEntity<Void> leaveWork(@LoginUser LoginUserEmail loginUserEmail) {
-        timeRecordService.leaveWork(loginUserEmail.getEmail());
+    public ResponseEntity<Void> leaveWork(@LoginUser AuthInfo authInfo) {
+        timeRecordService.leaveWork(authInfo.getUserId());
         return ResponseEntity.ok()
                              .build();
     }
@@ -39,14 +39,14 @@ public class TimeRecordController {
     /**
      * 해당 날짜의 사용자의 출퇴근 기록을 조회
      *
-     * @param date           조회 날짜
-     * @param loginUserEmail 토큰 검증이 완료된 사용자의 email
+     * @param date     조회 날짜
+     * @param authInfo 토큰 검증이 완료된 사용자의 정보
      */
     @GetMapping(value = "/me")
     public ResponseEntity<TimeRecordResponse> findMyTimeRecordByDate(
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
-            @LoginUser LoginUserEmail loginUserEmail) {
+            @LoginUser AuthInfo authInfo) {
         return ResponseEntity.ok()
-                             .body(timeRecordService.findTimeRecordByDate(loginUserEmail.getEmail(), date));
+                             .body(timeRecordService.findTimeRecordByDate(authInfo.getUserId(), date));
     }
 }
--- src/main/java/com/douzone/timeattendance/service/TimeRecordService.java
+++ src/main/java/com/douzone/timeattendance/service/TimeRecordService.java
@@ -1,15 +1,12 @@
 package com.douzone.timeattendance.service;
 
 import com.douzone.timeattendance.domain.TimeRecord;
-import com.douzone.timeattendance.domain.User;
 import com.douzone.timeattendance.dto.timerecord.TimeRecordResponse;
 import com.douzone.timeattendance.dto.timerecord.TimeRecordUpdateDto;
 import com.douzone.timeattendance.exception.timerecord.AlreadyExistsWorkRecordException;
-import com.douzone.timeattendance.exception.timerecord.InvalidLeaveWorkException;
+import com.douzone.timeattendance.exception.timerecord.ImpossibleLeaveWorkException;
 import com.douzone.timeattendance.exception.timerecord.NoSuchTimeRecordException;
-import com.douzone.timeattendance.exception.user.NoSuchUserException;
 import com.douzone.timeattendance.mapper.TimeRecordMapper;
-import com.douzone.timeattendance.mapper.UserMapper;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import lombok.RequiredArgsConstructor;
@@ -21,29 +18,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TimeRecordService {
 
-    private final UserMapper userMapper;
     private final TimeRecordMapper timeRecordMapper;
 
     /**
      * 요청 당일의 사용자의 출근 기록을 생성합니다.
      *
-     * @param email 로그인 한 사용자의 이메일
+     * @param userId 사용자 id
      */
-    public void startWork(String email) {
-        User user = userMapper.findByEmail(email)
-                              .orElseThrow(NoSuchUserException::new);
-
+    public void startWork(Long userId) {
         //TODO: 특정 위치 및 IP만 출퇴근가능
         //TODO: 배포 여부 검증
 
         //중복 출근 검증
-        if (existsWorkRecord(user.getUserId(), LocalDate.now())) {
+        if (existsWorkRecord(userId, LocalDate.now())) {
             throw new AlreadyExistsWorkRecordException();
         }
 
         //새 출근 기록 생성
         TimeRecord timeRecord = TimeRecord.builder()
-                                          .userId(user.getUserId())
+                                          .userId(userId)
                                           .date(LocalDate.now())
                                           .work(LocalTime.now())
                                           .workState("미처리") //TODO: enum 사용
@@ -55,22 +48,20 @@ public class TimeRecordService {
     /**
      * 요청 당일의 사용자의 퇴근 기록을 업데이트합니다.
      *
-     * @param email 로그인 한 사용자의 이메일
+     * @param userId 사용자 id
      */
-    public void leaveWork(String email) {
-        User user = userMapper.findByEmail(email)
-                              .orElseThrow(NoSuchUserException::new);
-
+    public void leaveWork(Long userId) {
         //TODO: 특정 위치 및 IP만 출퇴근가능
+        //TODO: 배포 여부 검증
 
         //출근기록 존재여부 검증
-        if (!existsWorkRecord(user.getUserId(), LocalDate.now())) {
-            throw new InvalidLeaveWorkException();
+        if (!existsWorkRecord(userId, LocalDate.now())) {
+            throw new ImpossibleLeaveWorkException();
         }
 
         //출퇴근 테이블에 퇴근 기록 업데이트
         TimeRecordUpdateDto updateParam = TimeRecordUpdateDto.builder()
-                                                             .userId(user.getUserId())
+                                                             .userId(userId)
                                                              .date(LocalDate.now())
                                                              .leaveWork(LocalTime.now())
                                                              .build();
@@ -78,11 +69,8 @@ public class TimeRecordService {
     }
 
     @Transactional(readOnly = true)
-    public TimeRecordResponse findTimeRecordByDate(String email, LocalDate date) {
-        User user = userMapper.findByEmail(email)
-                              .orElseThrow(NoSuchUserException::new);
-
-        return timeRecordMapper.findByDate(user.getUserId(), date)
+    public TimeRecordResponse findTimeRecordByDate(Long userId, LocalDate date) {
+        return timeRecordMapper.findByDate(userId, date)
                                .map(TimeRecordResponse::new)
                                .orElseThrow(NoSuchTimeRecordException::new);
     }
