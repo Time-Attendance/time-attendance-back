aa67764616868df2dffd27a52a41e13f8c8aa2ad
crupy <wlxh95@naver.com>
2023-09-04 15:00:55 +0900

    [Feat] 정산 로직 1차 세팅
    정산에서 회원 조회 로직까지 구현

---
+++ src/main/java/com/douzone/timeattendance/dto/settlement/SettlementSearchDto.java
@@ -0,0 +1,11 @@
+package com.douzone.timeattendance.dto.settlement;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class SettlementSearchDto {
+
+    private Long companyId;
+    private Long workGroupId;
+}
--- src/main/java/com/douzone/timeattendance/mapper/SettlementMapper.java
+++ src/main/java/com/douzone/timeattendance/mapper/SettlementMapper.java
@@ -1,11 +1,10 @@
 package com.douzone.timeattendance.mapper;
 
-import com.douzone.timeattendance.domain.Settlement;
-import com.douzone.timeattendance.dto.settlement.SettlementFindRequest;
-import com.douzone.timeattendance.dto.settlement.SettlementResponse;
-import com.douzone.timeattendance.dto.settlement.SettlementUpdateRequest;
+import com.douzone.timeattendance.dto.settlement.*;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Mapper
@@ -14,5 +13,10 @@ public interface SettlementMapper {
     List<SettlementResponse> findAll(SettlementFindRequest settlementFindRequest);
 
     void updateSettlement(SettlementUpdateRequest settlementUpdateRequest);
-    void insertSettlement(Settlement settlement);
+
+    //회사와 근무그룹에 맞는 유저 목록들 조회
+    List<SettlementFindCompanyDto> findCompanyAndWorkGroup(@Param("date")LocalDate date, @Param("searchParam") SettlementSearchDto settlementSearchDto);
+
+    //회사, 근무그룹 찾기
+    List<SettlementSearchDto> findCompanyIdAndWorkGroupIdList();
 }
