95cf094594ab476da6c3e74ae0b2b8e2cd66ce73
crupy <wlxh95@naver.com>
2023-09-20 22:18:01 +0900

    [Fix] 정산 로직 수정
    정산 로직 수정

--- src/test/java/com/douzone/timeattendance/TimeAttendanceApplicationTests.java
+++ src/test/java/com/douzone/timeattendance/TimeAttendanceApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.test.annotation.Rollback;
 
 @SpringBootTest
 class TimeAttendanceApplicationTests {
@@ -13,6 +14,7 @@ class TimeAttendanceApplicationTests {
 	private SettlementService settlementService;
 
 	@Test
+	@Rollback
 	void contextLoads() {
 		settlementService.settlementSchedule();
 	}
