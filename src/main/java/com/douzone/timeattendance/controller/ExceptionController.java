4e93cf5167335a0f9e4029d94387f7f3cb37a61c
hunbk <shyun823@gmail.com>
2023-09-23 01:09:25 +0900

    [Fix] 서버 인증 실패 시, 응답 상태 코드 401로 설정

--- src/main/java/com/douzone/timeattendance/controller/ExceptionController.java
+++ src/main/java/com/douzone/timeattendance/controller/ExceptionController.java
@@ -1,8 +1,10 @@
 package com.douzone.timeattendance.controller;
 
 import com.douzone.timeattendance.exception.auth.UnauthorizedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -10,6 +12,7 @@ public class ExceptionController {
     /**
      * 서버 인증 예외 처리
      */
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(UnauthorizedException.class)
     public String unauthorizedException(UnauthorizedException e) {
         return "error/403";
